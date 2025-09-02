49b4b97cab9336c26d1a91c916df8f5aec45d73f
Helena Alfonso <alfonsoveramaria@gmail.com>
2025-09-02 14:16:58 -0500

    v0.12 productohome clase homeusercontroller method productohome 020925

--- src/main/java/com/example/ecommerce/controller/HomeUserController.java
+++ src/main/java/com/example/ecommerce/controller/HomeUserController.java
@@ -1,18 +1,27 @@
 package com.example.ecommerce.controller;
 
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.ecommerce.model.Producto;
 import com.example.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeUserController {
-	//instancia de productoService
+
+	// LOGGER
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(HomeUserController.class);
+
+	// instancia de producto service
 	@Autowired
 	private IProductoService productoService;
 
@@ -20,5 +29,18 @@ public class HomeUserController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+
+	// metodo que carga el producto del usuario con el id producto
+	@GetMapping("productohome/{id}")
+	public String productohome(@PathVariable Integer id, Model model) {
+		LOGGER.info("Id producto enciado como parametro{}", id);
+		Producto p = new Producto();
+		Optional<Producto> op = productoService.get(id);
+
+		p = op.get();
+		model.addAttribute("producto", p);
+
+		return "usuario/productohome";
 	}
 }
